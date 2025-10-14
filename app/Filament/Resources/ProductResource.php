5c9c7200fff7878883eee91822240bbd7d479b6d
harsh-agr79 <agrharsh7932@gmail.com>
2025-10-14 14:35:40 +0530

    review relation manager

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -119,7 +119,7 @@ class ProductResource extends Resource {
 
     public static function getRelations(): array {
         return [
-            //
+            RelationManagers\ReviewsRelationManager::class,
         ];
     }
 
---
+++ app/Filament/Resources/ProductResource/RelationManagers/ReviewsRelationManager.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Filament\Resources\ProductResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ReviewsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'reviews';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('review')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Select::make('rating')
+                    ->options([
+                        1 => '1 Star',
+                        2 => '2 Stars',
+                        3 => '3 Stars',
+                        4 => '4 Stars',
+                        5 => '5 Stars',
+                    ])
+                    ->required(),
+                Forms\Components\Select::make('user_id')
+                    ->relationship('user', 'name')
+                    ->required(),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('review')
+            ->columns([
+                Tables\Columns\TextColumn::make('review'),
+                Tables\Columns\TextColumn::make('rating'),
+                Tables\Columns\TextColumn::make('user.name')->label('User'),
+                Tables\Columns\TextColumn::make('created_at')->dateTime()->label('Reviewed At'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
